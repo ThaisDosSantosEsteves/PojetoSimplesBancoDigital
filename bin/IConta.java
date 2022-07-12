fc1e266a88425c73d46a29d7b9e04909abb126e5
Thais Esteves <thaisdossantosesteves@gmail.com>
2022-07-12 17:04:44 -0300

    commit inicial

---
+++ bin/IConta.java
@@ -0,0 +1,11 @@
+package bin;
+
+public interface IConta {
+     void sacar(double valor);
+     void depositar(double valor);
+     void transferir(double valor, IConta contaDestino);
+
+     void imprimirExtrato();
+
+
+}
